2ad191e648ab2fd467fcd9b132c9506c2575058a
Lougle <louglescom@gmail.com>
2021-12-29 00:11:17 +0200

    added static method

--- module05.js
+++ module05.js
@@ -280,3 +280,29 @@ console.log("", iphone.qwerty);
 
 iphone.qwerty = "Iphone11ProMax";
 console.log(iphone.qwerty);
+
+console.log("Статические свойства и методы");
+// Класс со статическими свойствами и методами
+class Calc {
+  // Класс-калькулятор для двух аргументов
+  constructor() {}
+
+  // Метод как замена свойству
+  static get PI() {
+    return 3.14;
+  }
+
+  // Статический метод +
+  static add(...args) {
+    return args.reduce((acc, next) => acc + next, 0);
+  }
+
+  // Статический метод *
+  static mult(...args) {
+    return args.reduce((acc, next) => acc * next, 1);
+  }
+}
+
+console.log(Calc.PI); // 3.14
+console.log(Calc.add(2, 3, 4)); // 9
+console.log(Calc.mult(12, 3, 4)); // 144
